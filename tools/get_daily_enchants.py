a23fcb1ee525bf61b8bae5e39bf03ecb9f8a1db1
LawTotem <lawtotem8511@gmail.com>
2021-10-21 03:53:20 -0400

    Started improving daily enchants.
    Added a title line to aid when importing into google sheets/excel.
    Added league support.
    Added date stamp so multiple daily grabs can be appended and the most up to date version of duplicate characters used.
    Added exception handling if URL is too hard.

--- tools/get_daily_enchants.py
+++ tools/get_daily_enchants.py
@@ -19,6 +19,7 @@ import urllib.request
 from urllib.parse import quote
 import time
 import random
+import datetime
 
 # You need to get your own api key
 # You have to open poe.ninja with the browsers developer tools
@@ -26,58 +27,81 @@ import random
 # Should look like
 # https://poe.ninja/api/data/API_KEY_HERE/getbuildoverview?overview=expedition&type=exp&language=en
 # I couldn't find a cookie that had it
-key = 'API_KEY_HERE';
+# In Chrome
+# three little dots ->more tools->developer tools 
+# or maybe Ctrl+Shift+ I 
+# then the network tab
+# click the getbuildoverview request when selecting "builds" on the site
+# should give you a request URL
 
-general_url = 'https://poe.ninja/api/data/' + key + '/getbuildoverview?overview=daily&type=mix&language=en';
+# According to @Skid they rotate daily bot may not be invalid.
+key = 'b1ead41788cede02ff8f49656c736490';
+
+# I recommend daily but you can use the league name to pull the top Exp characters.
+league = 'expedition';
+
+if league == 'daily' :
+    s_type = 'mix';
+else :
+    s_type = 'exp';
+
+
+general_url = 'https://poe.ninja/api/data/' + key + '/getbuildoverview?overview=' + league + '&type=' + s_type + '&language=en';
 general_req = urllib.request.Request(general_url, headers={'User-Agent': 'Mozilla/5.0'});
 general_page = urllib.request.urlopen(general_req).read()
 general_json = json.loads(general_page)
 time.sleep(0.2)
 
-# Helm Item Class 2,3
-# Helm Item Slot 1,1
-
-# Frame Type 2 (elder?)
-# TypeName = Bone Helm
-# BaseType = Bone Helm
-
-# Frame Type 3 unique?
-# Frame Type 2 
-
-outfile = open('raw_enchants.txt','w')
+now_time = datetime.datetime.now();
+outfile = open('raw_enchants' + now_time.strftime("%j_%H") + '.txt','w')
 
 all_select = list(zip(general_json['accounts'], general_json['names']));
 
-sub_select = all_select[0:100]
+# We save account and character in case we need to look them up later
+# Helm Name is in case we mess up unique identification
+# Helm Base and ilvl is so we can get some idea as to what is important
+# Is Unique just flags if the helm is a unique helm
+# Is Elder - this influence cannot be added later and may be needed
+# Is Shaper - this influence cannot be added later and may be needed
+# Time - In case you are joining multiple files togather you can take the most recent version of a duplicate character
+outfile.write(":".join(["Account","Character","Helm Name","Helm Base","Helm ilvl","Enchant","Is Unique","Is Elder","Is Shaper","Time"]))
+
+time_str = now_time.strftime("%j.%H");
+
+sub_select = all_select[0:10]
+# When testing the script consider substituting sub_select for all_select
 for account_name in all_select :
-    p_url = 'https://poe.ninja/api/data/' + key + '/GetCharacter?account=' + quote(account_name[0].encode('utf-8')) + '&name=' + quote(account_name[1].encode('utf-8')) + '&overview=daily&type=mix&language=en';
-    p_req = urllib.request.Request(p_url, headers={'User-Agent': 'Mozilla/5.0'});
-    p_page = urllib.request.urlopen(p_req).read();
-    p_json = json.loads(p_page)
     try :
-        helm_index = [ii['itemSlot'] == 1 for ii in p_json['items']].index(True);
+        p_url = 'https://poe.ninja/api/data/' + key + '/GetCharacter?account=' + quote(account_name[0].encode('utf-8')) + '&name=' + quote(account_name[1].encode('utf-8')) + '&overview=' + league + '&type=' + s_type + '&language=en';
+        p_req = urllib.request.Request(p_url, headers={'User-Agent': 'Mozilla/5.0'});
+        p_page = urllib.request.urlopen(p_req).read();
+        p_json = json.loads(p_page)
+        try :
+            helm_index = [ii['itemSlot'] == 1 for ii in p_json['items']].index(True);
+        except :
+            helm_index = -1;
+        if helm_index >= 0 :
+            helm = p_json['items'][helm_index]['itemData'];
+            name = helm['name'];
+            base = helm['baseType'];
+            ilvl = str(helm['ilvl']);
+            try :
+                enchant = helm['enchantMods'][0];
+            except :
+                enchant = 'None'
+            is_unique = str(helm['frameType'] == 3);
+            try :
+                influences = helm['influences']
+                is_elder = str(influences['elder'])
+                is_shaper = str(influences['shaper'])
+            except :
+                is_elder = str(False);
+                is_shaper = str(False);
+            if enchant != 'None' :
+                outfile.write(":".join([account_name[0], account_name[1], name, base, ilvl, enchant, is_unique, is_elder, is_shaper, time_str]))
+                outfile.write("\n");
     except :
-        helm_index = -1;
-    if helm_index >= 0 :
-        helm = p_json['items'][helm_index]['itemData'];
-        name = helm['name'];
-        base = helm['baseType'];
-        ilvl = str(helm['ilvl']);
-        try :
-            enchant = helm['enchantMods'][0];
-        except :
-            enchant = 'None'
-        is_unique = str(helm['frameType'] == 3);
-        try :
-            influences = helm['influences']
-            is_elder = str(influences['elder'])
-            is_shaper = str(influences['shaper'])
-        except :
-            is_elder = str(False);
-            is_shaper = str(False);
-        if enchant != 'None' :
-            outfile.write(":".join([account_name[0], account_name[1], name, base, ilvl, enchant, is_unique, is_elder, is_shaper]))
-            outfile.write("\n");
+        print('Could not fetch data for:' + account_name[0] + ' -- ' + account_name[1]);
     time.sleep(0.2 + random.randint(0,50)/100);
 
 outfile.close()
